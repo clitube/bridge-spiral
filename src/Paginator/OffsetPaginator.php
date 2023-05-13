edb613b74ab578e0f65d159e0680be6a967265d8
roxblnfk <roxblnfk@ya.ru>
2023-05-14 02:04:50 +0400

    Simplify `OffsetPaginator`

--- src/Paginator/OffsetPaginator.php
+++ src/Paginator/OffsetPaginator.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace CliTube\Bridge\Spiral\Paginator;
 
+use CliTube\Support\Pagination\BaseOffsetPaginator;
 use Closure;
 use Countable;
 use IteratorAggregate;
@@ -13,27 +14,25 @@ use Traversable;
 /**
  * Paginator Bridge from Spiral to CliTube
  *
- * @template TPaginator of PaginableInterface&IteratorAggregate&Countable
+ * @template TPaginator of Countable&IteratorAggregate&PaginableInterface
  * @template TItemType
  */
-class OffsetPaginator implements \CliTube\Contract\Pagination\OffsetPaginator
+class OffsetPaginator extends BaseOffsetPaginator
 {
-    private int $limit = 1;
-    private int $offset = 0;
-    private int $page = 1;
-    private ?int $pages = null;
-    private ?int $count = null;
-
-    private ?array $buffer = null;
-
     /** @var null|Closure(TItemType $item): array<array-key, scalar> */
     private ?Closure $itemConverter = null;
+
     /** @var null|Closure(TPaginator $paginator): ?int */
     private ?Closure $countCalculator = null;
 
     private function __construct(
-        private PaginableInterface&IteratorAggregate&Countable $paginator,
+        private Countable&IteratorAggregate&PaginableInterface $paginator,
     ) {
+    }
+
+    public function __clone()
+    {
+        $this->paginator = clone $this->paginator;
     }
 
     public static function create(PaginableInterface&IteratorAggregate&Countable $paginator): static
@@ -72,54 +71,23 @@ class OffsetPaginator implements \CliTube\Contract\Pagination\OffsetPaginator
             foreach ($this->getContent() as $item) {
                 yield ($this->itemConverter)($item);
             }
-        } else {
-            yield from $this->getContent();
+            return;
         }
+
+        yield from $this->getContent();
     }
 
     public function withOffset(int $offset): static
     {
-        $clone = clone $this;
+        $clone = parent::withOffset($offset);
         $clone->paginator->offset($offset);
-        $clone->offset = $offset;
-        $clone->calc();
         return $clone;
-    }
-
-    public function getOffset(): int
-    {
-        return $this->offset;
     }
 
     public function withLimit(int $limit): static
     {
-        $clone = clone $this;
+        $clone = parent::withLimit($limit);
         $clone->paginator->limit($limit);
-        $clone->limit = $limit;
-        $clone->calc();
-        return $clone;
-    }
-
-    public function getLimit(): int
-    {
-        return $this->limit;
-    }
-
-    public function nextPage(): static
-    {
-        $clone = clone $this;
-        $clone->offset = $clone->pages === null
-            ? $clone->offset + $clone->limit
-            : \min(($clone->pages - 1) * $clone->limit, $clone->limit * $clone->page);
-        $clone->calc();
-        return $clone;
-    }
-
-    public function previousPage(): static
-    {
-        $clone = clone $this;
-        $clone->offset = \max(0, $clone->limit * ($clone->page - 2));
-        $clone->calc();
         return $clone;
     }
 
@@ -128,29 +96,11 @@ class OffsetPaginator implements \CliTube\Contract\Pagination\OffsetPaginator
         if ($this->count === null && $this->countCalculator !== null) {
             $this->count = ($this->countCalculator)(clone $this->paginator);
         }
-        return $this->count;
+
+        return parent::getCount();
     }
 
-    public function count(): int
-    {
-        return \count($this->getContent());
-    }
-
-    public function __clone(): void
-    {
-        $this->paginator = clone $this->paginator;
-    }
-
-    private function calc(): void
-    {
-        $this->buffer = null;
-        if ($this->count !== null) {
-            $this->pages = \max(1, (int)\ceil($this->count / $this->limit));
-        }
-        $this->page = (int)\ceil($this->offset / $this->limit) + 1;
-    }
-
-    private function getContent(): array
+    protected function getContent(): array
     {
         $this->buffer ??= \iterator_to_array($this->paginator->getIterator(), false);
         return $this->buffer;
